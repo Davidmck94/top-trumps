bbeaf168c8f1423815713f4a0a5a8c36266a8cf8
Davidmck94 <david.mckiernan@barclays.com>
2022-08-02 15:11:14 +0100

    Final Version

--- script.js
+++ script.js
@@ -1,1 +1,531 @@
+// Collection of 30 Pokemon cards
 
+const cards = [
+    {   id: 1, 
+        name: "Bulbasaur",
+        HP: 45, 
+        Attack: 49, 
+        Defense: 49, 
+        SpAttack: 65, 
+        SpDefence: 65, 
+        Speed: 45, 
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/001.png"
+        },
+    {   id: 2, 
+        name: "Ivysaur", 
+        HP: 60, 
+        Attack: 62, 
+        Defense: 63, 
+        SpAttack: 80, 
+        SpDefence: 80, 
+        Speed: 60, 
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/002.png",
+        },
+    {   id: 3, 
+        name: "Venusaur", 
+        HP: 80, 
+        Attack: 82, 
+        Defense: 83, 
+        SpAttack: 100, 
+        SpDefence: 100, 
+        Speed: 80, 
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/003.png",
+        },
+    {   id: 4, 
+        name: "Charmander", 
+        HP: 39, 
+        Attack: 52,  
+        Defense: 43, 
+        SpAttack: 60, 
+        SpDefence: 50, 
+        Speed: 65, 
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/004.png",
+        },
+    {   id: 5,
+        name: "Charmeleon",
+        HP: 58,
+        Attack: 64,
+        Defense: 58,
+        SpAttack: 80,
+        SpDefence: 65,
+        Speed: 80,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/005.png",
+        },
+
+    {   id: 6,
+        name: "Charizard",
+        HP: 78,
+        Attack: 84,
+        Defense: 78,
+        SpAttack: 109,
+        SpDefence: 85,
+        Speed: 100,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/006.png",
+        },
+    {   id: 7,
+        name: "Squirtle",
+        HP: 44,
+        Attack: 48,
+        Defense: 65,
+        SpAttack: 50,
+        SpDefence: 64,
+        Speed: 43,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/007.png",
+        },
+    {   id: 8,
+        name: "Wartortle",
+        HP: 59,
+        Attack: 63,
+        Defense: 80,
+        SpAttack: 65,
+        SpDefence: 80,
+        Speed: 58,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/008.png",
+        },
+    {   id: 9,
+        name: "Blastoise",
+        HP: 79,
+        Attack: 83,
+        Defense: 100,
+        SpAttack: 85,
+        SpDefence: 105,
+        Speed: 78,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/009.png",
+        },
+    {   id: 10,
+        name: "Caterpie",
+        HP: 45,
+        Attack: 30,
+        Defense: 35,
+        SpAttack: 20,
+        SpDefence: 20,
+        Speed: 45,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/010.png",
+        },
+    {   id: 11,
+        name: "Metapod",
+        HP: 50,
+        Attack: 20,
+        Defense: 55,
+        SpAttack: 25,
+        SpDefence: 25,
+        Speed: 30,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/011.png",
+        },
+    {   id: 12,
+        name: "Butterfree",
+        HP: 60,
+        Attack: 45,
+        Defense: 50,
+        SpAttack: 90,
+        SpDefence: 80,
+        Speed: 70,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/012.png",
+        },
+    {   id: 13,
+        name: "Weedle",
+        HP: 40,
+        Attack: 35,
+        Defense: 30,
+        SpAttack: 20,
+        SpDefence: 20,
+        Speed: 50,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/013.png",
+        },
+    {   id: 14,
+        name: "Kakuna",
+        HP: 45,
+        Attack: 25,
+        Defense: 50,
+        SpAttack: 25,
+        SpDefence: 25,
+        Speed: 35,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/014.png",
+        },
+    {   id: 15,
+        name: "Beedrill",
+        HP: 65,
+        Attack: 90,
+        Defense: 40,
+        SpAttack: 45,
+        SpDefence: 80,
+        Speed: 75,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/015.png",
+        },
+    {   id: 16,
+        name: "Pidgey",
+        HP: 40,
+        Attack: 45,
+        Defense: 40,
+        SpAttack: 35,
+        SpDefence: 35,
+        Speed: 56,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/016.png",
+        },
+    {   id: 17,
+        name: "Pidgeotto",
+        HP: 63,
+        Attack: 60,
+        Defense: 55,
+        SpAttack: 50,
+        SpDefence: 50,
+        Speed: 71,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/017.png",
+        },
+    {   id: 18,
+        name: "Pidgeot",
+        HP: 83,
+        Attack: 80,
+        Defense: 75,
+        SpAttack: 70,
+        SpDefence: 70,
+        Speed: 101,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/018.png",
+        },
+    {   id: 19,
+        name: "Rattata",
+        HP: 30,
+        Attack: 56,
+        Defense: 35,
+        SpAttack: 25,
+        SpDefence: 35,
+        Speed: 72,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/019.png",
+        },
+    {   id: 20,
+        name: "Raticate",
+        HP: 55,
+        Attack: 81,
+        Defense: 60,
+        SpAttack: 50,
+        SpDefence: 70,
+        Speed: 97,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/020.png",
+        },
+    {   id: 21,
+        name: "Spearow",
+        HP: 40,
+        Attack: 60,
+        Defense: 30,
+        SpAttack: 31,
+        SpDefence: 31,
+        Speed: 70,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/021.png",
+        },
+    {   id: 22,
+        name: "Fearow",
+        HP: 65,
+        Attack: 90,
+        Defense: 65,
+        SpAttack: 61,
+        SpDefence: 61,
+        Speed: 100,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/022.png",
+        },
+    {   id: 23,
+        name: "Ekans",
+        HP: 35,
+        Attack: 60,
+        Defense: 44,
+        SpAttack: 40,
+        SpDefence: 54,
+        Speed: 55,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/023.png",
+        },
+    {   id: 24,
+        name: "Arbok",
+        HP: 60,
+        Attack: 95,
+        Defense: 69,
+        SpAttack: 65,
+        SpDefence: 79,
+        Speed: 80,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/024.png",
+        },
+    {   id: 25,
+        name: "Pikachu",
+        HP: 35,
+        Attack: 55,
+        Defense: 40,
+        SpAttack: 50,
+        SpDefence: 50,
+        Speed: 90,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/025.png",
+        },
+    {   id: 26,
+        name: "Raichu",
+        HP: 60,
+        Attack: 90,
+        Defense: 55,
+        SpAttack: 90,
+        SpDefence: 80,
+        Speed: 110,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/026.png",
+        },
+    {   id: 27,
+        name: "Sandshrew",
+        HP: 50,
+        Attack: 75,
+        Defense: 85,
+        SpAttack: 20,
+        SpDefence: 30,
+        Speed: 40,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/027.png",
+        },
+    {   id: 28,
+        name: "Sandslash",
+        HP: 75,
+        Attack: 100,
+        Defense: 110,
+        SpAttack: 45,
+        SpDefence: 55,
+        Speed: 65,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/028.png",
+        },
+    {   id: 29,
+        name: "Nidoran",
+        HP: 55,
+        Attack: 47,
+        Defense: 52,
+        SpAttack: 40,
+        SpDefence: 40,
+        Speed: 41,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/029.png",
+        },
+    {   id: 30,
+        name: "Nidorina",
+        HP: 70,
+        Attack: 62,
+        Defense: 67,
+        SpAttack: 55,
+        SpDefence: 55,
+        Speed: 56,
+        img: "https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/images/pokedex/hires/030.png",
+    },
+]
+
+// Const variables set for the button comparisons
+
+const hpBtn = document.getElementById("HP-btn");
+const attackBtn = document.getElementById("Attack-btn");
+const defenceBtn = document.getElementById("Defence-btn");
+const spAttackBtn = document.getElementById("Special-Attack-btn");
+const spDefenceBtn = document.getElementById("Special-Defence-btn");
+const speedBtn = document.getElementById("Speed-btn");
+const playAgain = document.getElementById("play-again-btn")
+
+// Final outcome of game display message
+
+let msgDisplay = document.getElementById("message-display");
+
+// Player and CPU cards
+
+let player1Cards = [];
+let cpuCards = [];
+
+// Player 1 and CPU score 
+
+let player1Score = 0; 
+let cpuScore = 0; 
+
+//Player and CPU current cards
+
+let player1Current;
+let cpuCurrent;
+
+// Divide the deck into two equal piles of 15 each
+
+let player1ScoreToBegin = 0;
+let cpuScoreToBegin = 0;
+
+
+// Function to shuffle the pile of 30 cards and distribute
+
+function shuffleCards() {
+    let totalCards  = cards.length;
+    let player1TotalCards = 0;
+    let cpuTotalCards = 0;
+
+    while(--totalCards >= 0) {
+        let cardIdx = Math.floor(Math.random() * (totalCards +1));
+        let randomisedCard = cards.splice(cardIdx, 1);
+
+            if(player1TotalCards == cpuTotalCards) {
+                player1Cards.push(randomisedCard[0]);
+                player1TotalCards +=1;
+                cpuScoreToBegin +=1;
+            } else if(player1TotalCards > cpuTotalCards) {
+                cpuCards.push(randomisedCard[0]);
+                cpuTotalCards +=1;
+                player1ScoreToBegin +=1;
+            }
+    }
+}
+shuffleCards()
+
+// Variables set to show the live scores of the players
+
+let player1LiveScore = document.getElementById("score-Player1");
+let cpuLiveScore = document.getElementById("score-Cpu");
+
+//Display starter scores
+
+player1LiveScore.innerText = player1ScoreToBegin
+cpuLiveScore.innerText = cpuScoreToBegin
+
+function distributeCards(){
+    player1Current = player1Cards.shift();
+    cpuCurrent = cpuCards.shift();
+}
+
+distributeCards()
+
+//Variable set to display the current cards of both players
+
+let player1LiveCard = document.getElementById("player1-card-img");
+let p2CardDisplay = document.getElementById("cpu-card-img");
+
+//Accessing the different Pokemon attributes for comparison purposes
+
+function player1Attributes(){
+    player1LiveCard.src  = `${player1Current.img}`;
+    document.getElementById('player1-pokemon').innerText = player1Current.name;
+    document.getElementById('player1-Hp').innerText = player1Current.HP;
+    document.getElementById('player1-Attack').innerText = player1Current.Attack;
+    document.getElementById('player1-Defence').innerText = player1Current.Defense;
+    document.getElementById('player1-SpecialAttack').innerText = player1Current.SpAttack;
+    document.getElementById('player1-SpecialDefense').innerText= player1Current.SpDefence;
+    document.getElementById('player1-Speed').innerText = player1Current.Speed;
+}
+player1Attributes()
+
+function cpuAttributes(){
+    p2CardDisplay.src  = `${cpuCurrent.img}`;
+    document.getElementById('cpuPokemon-name').innerText = cpuCurrent.name;
+    document.getElementById('cpu-Hp').innerText = cpuCurrent.HP;
+    document.getElementById('cpu-Attack').innerText = cpuCurrent.Attack;
+    document.getElementById('cpu-Defence').innerText = cpuCurrent.Defense;
+    document.getElementById('cpu-SpecialAttack').innerText = cpuCurrent.SpAttack;
+    document.getElementById('cpu-SpecialDefence').innerText= cpuCurrent.SpDefence;
+    document.getElementById('cpu-Speed').innerText = cpuCurrent.Speed;
+}
+cpuAttributes()
+
+// If either player reaches a card count of zero, produce following message.
+
+function gameOutcome(){
+    if (cpuCards.length == 0 ){
+        msgDisplay.textContent = "Well Done! You caught them all!"
+    } else if (player1Cards.length == 0)
+        msgDisplay.textContent = "Better luck next time!"
+}
+
+gameOutcome()
+
+//Through each round, a comparison of attributes takes place and an outcome is determined.
+
+function processResult(outcome){
+
+    if(outcome == 'player1-Wins'){
+        
+        player1Cards.push(player1Current)
+        player1Cards.push(cpuCurrent)
+        player1ScoreToBegin = player1Cards.length;
+        cpuScoreToBegin = cpuCards.length;
+        player1LiveScore.innerText = player1ScoreToBegin
+        cpuLiveScore.innerText = cpuScoreToBegin
+
+        if (player1Cards.length && cpuCards.length >= 1){
+            distributeCards()
+            player1Attributes()
+            cpuAttributes()
+        } else {
+            gameOutcome()
+        }
+    }
+
+     if (outcome == 'cpu-Wins'){
+        
+        cpuCards.push(player1Current)
+        cpuCards.push(cpuCurrent)
+        player1ScoreToBegin = player1Cards.length;
+        cpuScoreToBegin= cpuCards.length;
+        player1LiveScore.innerText = player1ScoreToBegin
+        cpuLiveScore.innerText = cpuScoreToBegin
+
+        if (player1Cards.length && cpuCards.length >= 1){
+            distributeCards()
+            player1Attributes()
+            cpuAttributes()
+        } else {
+            gameOutcome()
+        }
+    }
+}
+
+// Buttons to compare the attributes of both pokemon cards
+
+    playAgain.addEventListener('click', playAgainReset);
+        function playAgainReset() {
+        // player1Score = 0;
+        // cpuScore = 0;
+        // shuffleCards()
+        // distributeCards()
+        // player1Attributes()
+        // cpuAttributes()
+
+    }
+
+    hpBtn.addEventListener('click', hpAttr);
+        function hpAttr(){
+        if(player1Current.HP > cpuCurrent.HP){
+            processResult('player1-Wins')
+        } else {
+            processResult('cpu-Wins')
+        }
+    }
+
+    attackBtn.addEventListener('click', attackAttr);
+        function attackAttr(){
+        if(player1Current.Attack > cpuCurrent.Attack){
+            processResult('player1-Wins')
+        } else {
+            processResult('cpu-Wins')
+        }
+    }
+
+    defenceBtn.addEventListener('click', defenceAttr);
+        function defenceAttr(){
+        if(player1Current.Defense > cpuCurrent.Defense){
+            processResult('player1-Wins')
+        } else {
+            processResult('cpu-Wins')
+        }
+}
+
+    spAttackBtn.addEventListener('click', spAttackAttr);
+        function spAttackAttr(){
+        if(player1Current.SpAttack > cpuCurrent.SpAttack){
+            processResult('player1-Wins')
+        } else {
+            processResult('cpu-Wins')
+        }
+}
+
+    spDefenceBtn.addEventListener('click', spDefenceAttr);
+        function spDefenceAttr(){
+        if(player1Current.SpDefence > cpuCurrent.SpDefence){
+            processResult('player1-Wins')
+        } else {
+            processResult('cpu-Wins')
+        }
+}
+
+    speedBtn.addEventListener('click', speedAttr);
+        function speedAttr(){
+        if(player1Current.Speed > cpuCurrent.Speed){
+            processResult('player1-Wins')
+        } else {
+            processResult('cpu-Wins')
+        }
+}
